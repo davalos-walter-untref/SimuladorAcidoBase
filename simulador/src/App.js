1e046b395af323caca9c0f9386c0dfeaa008026f
unknown <davalos29789@estudiantes.untref.edu.ar>
2021-05-08 20:31:15 -0300

    reactapp + threejs

---
+++ simulador/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, { Component } from "react";
+import Simulador from "./components/simulador.js";
+class App extends Component {
+  render() {
+    return (
+      <Simulador>
+        </Simulador>
+    )
+  }
+}
+
+export default App
